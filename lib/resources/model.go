6a0ecc7324a15dbb255133c937a5f2197c886db8
Sean Barag <sean@barag.org>
2023-02-05 10:04:37 -0800

    layout: consolidate model -> Go rendering

--- lib/resources/model.go
+++ lib/resources/model.go
@@ -1,14 +1,6 @@
 package resources
 
 import (
-	"context"
-	"fmt"
-	"go/ast"
-	"go/token"
-
-	"terraform-provider-caiac/lib/astutil"
-	"terraform-provider-caiac/lib/tfutil"
-
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -30,51 +22,6 @@ var ImportSpec = schema.NestedBlockObject{
 			Required: true,
 		},
 	},
-}
-
-func MakeImportSpecAstNodes(ctx context.Context, in types.List) (ast.Decl, error) {
-	specs := []ast.Spec{}
-
-	elements := []types.Object{}
-	diags := in.ElementsAs(ctx, &elements, false)
-	if diags.HasError() {
-		return nil, fmt.Errorf("unable to cast ImportSpecs list to []types.Object: %+v", diags)
-	}
-
-	for _, val := range elements {
-		spec, err := ImportSpecToAst(ctx, val)
-		if err != nil {
-			return nil, err
-		}
-		specs = append(specs, spec)
-	}
-
-	return &ast.GenDecl{Tok: token.IMPORT, Specs: specs}, nil
-}
-
-func ImportSpecToAst(ctx context.Context, in types.Object) (*ast.ImportSpec, error) {
-	attr := in.Attributes()
-
-	var name *ast.Ident
-	nameStr, err := tfutil.AttrValueToString(ctx, attr["name"])
-	if err != nil {
-		return nil, err
-	}
-	if nameStr != "" {
-		name = ast.NewIdent(nameStr)
-	}
-
-	importPath, err := tfutil.AttrValueToString(ctx, attr["path"])
-	if err != nil {
-		return nil, err
-	}
-
-	out := &ast.ImportSpec{
-		Name: name,
-		Path: astutil.NewStringLiteral(importPath),
-	}
-
-	return out, nil
 }
 
 var FuncDecl = schema.NestedBlockObject{
--- lib/resources/render.go
+++ lib/resources/render.go
@@ -2,15 +2,20 @@ package resources
 
 import (
 	"context"
+	"fmt"
 	"go/ast"
 	"go/format"
+	"go/token"
 	"strings"
+	"terraform-provider-caiac/lib/astutil"
+	"terraform-provider-caiac/lib/tfutil"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 func renderGoSource(ctx context.Context, model *goSourceResourceModel, diags diag.Diagnostics) string {
-	imports, err := MakeImportSpecAstNodes(ctx, model.Imports)
+	imports, err := makeImportSpecAstNodes(ctx, model.Imports)
 	if err != nil {
 		diags.AddError(
 			"Error converting HCL to AST",
@@ -34,4 +39,49 @@ func renderGoSource(ctx context.Context, model *goSourceResourceModel, diags dia
 	}
 
 	return contents.String()
+}
+
+func makeImportSpecAstNodes(ctx context.Context, in types.List) (ast.Decl, error) {
+	specs := []ast.Spec{}
+
+	elements := []types.Object{}
+	diags := in.ElementsAs(ctx, &elements, false)
+	if diags.HasError() {
+		return nil, fmt.Errorf("unable to cast ImportSpecs list to []types.Object: %+v", diags)
+	}
+
+	for _, val := range elements {
+		spec, err := importSpecToAst(ctx, val)
+		if err != nil {
+			return nil, err
+		}
+		specs = append(specs, spec)
+	}
+
+	return &ast.GenDecl{Tok: token.IMPORT, Specs: specs}, nil
+}
+
+func importSpecToAst(ctx context.Context, in types.Object) (*ast.ImportSpec, error) {
+	attr := in.Attributes()
+
+	var name *ast.Ident
+	nameStr, err := tfutil.AttrValueToString(ctx, attr["name"])
+	if err != nil {
+		return nil, err
+	}
+	if nameStr != "" {
+		name = ast.NewIdent(nameStr)
+	}
+
+	importPath, err := tfutil.AttrValueToString(ctx, attr["path"])
+	if err != nil {
+		return nil, err
+	}
+
+	out := &ast.ImportSpec{
+		Name: name,
+		Path: astutil.NewStringLiteral(importPath),
+	}
+
+	return out, nil
 }
